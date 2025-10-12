0d594a10fd1dfa0fb7c9b999aef49926d3e36372
HaL4K <max_ytpropf@mail.ru>
2025-10-12 20:08:29 +0300

    add count metrics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,7 @@ export default function RootLayout({
       >
         {" "}
         {children}
+        <Script>{`yaCounter104282067.reachGoal('{{Scroll Depth Threshold}}');`}</Script>
         {/*Google Tag Manager*/}
         <Script id='google-tag-manager' strategy='afterInteractive'>
           {`
